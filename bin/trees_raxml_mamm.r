8a174c386ef2dca7af063ef33c7478d816fb81fb
cristoichkov <cristoichkov@gmail.com>
2019-09-06 12:51:15 -0500

    added trees_raxm_mamm.r

---
+++ bin/trees_raxml_mamm.r
@@ -0,0 +1,22 @@
+library(ggtree)
+library(ape)
+
+
+tree_run1 <- read.raxml("../data/raxml/clust_82_40_raxml/RAxML_bipartitionsBranchLabels.clust_82_40_run1")
+
+
+names <- read.csv("../meta/Mamm_names.csv", header = FALSE, sep = "\t")
+
+
+tree_run1 <- rename_taxa(tree_run1, names, V1, V2)
+
+
+ggtree(tree_run1) + geom_tiplab() + geom_label2(aes(label = bootstrap)) + geom_rootpoint()
+
+
+
+
+
+
+
+
