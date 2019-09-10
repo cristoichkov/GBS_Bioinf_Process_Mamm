a72e41852b95ab11164285ac9292f0c2fe6e26fb
cristoichkov <cristoichkov@gmail.com>
2019-09-10 17:30:02 -0500

    parameters added

--- bin/mindepth_parameters.sh
+++ bin/mindepth_parameters.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p keri
 #SBATCH -n 8
-#SBATCH --mem=48000
+#SBATCH --mem=56000
 
 export LD_LIBRARY_PATH=/opt/miniconda2/lib
 export HDF5_USE_FILE_LOCKING=FALSE
@@ -18,3 +18,4 @@ sed -i '/\[21] /c\'60'  ## [21] ' params-mindepth_2_2_80.txt
 
 # Run the assembly since step 4 until 7
 ipyrad -p params-mindepth_2_2_80.txt -s 4567 -f
+
