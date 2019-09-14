8d10a7d96ccf4ee4fde7f8ee876c2571e63c71f4
Cristian Cervantes <cristoichkov@gmail.com>
2019-09-14 08:56:02 -0500

    modified mindepth

--- bin/mindepth_parameters.sh
+++ bin/mindepth_parameters.sh
@@ -8,14 +8,21 @@ export LD_LIBRARY_PATH=/opt/miniconda2/lib
 export HDF5_USE_FILE_LOCKING=FALSE
 
 
-ipyrad -p params-default.txt -b mindepth_2_2_80
+ipyrad -p mindepth_2_2_80 -b mindepth_2_2_40
+
 
 # Modify the parameters of the created file
-sed -i '/\[12] /c\'2'  ## [12] ' params-mindepth_2_2_80.txt
-
-# Modify the parameters of the created file
-sed -i '/\[21] /c\'60'  ## [21] ' params-mindepth_2_2_80.txt
+sed -i '/\[21] /c\'30'  ## [21] ' params-mindepth_2_2_40.txt
 
 # Run the assembly since step 4 until 7
-ipyrad -p params-mindepth_2_2_80.txt -s 4567 -f
+ipyrad -p params-mindepth_2_2_40.txt -s 7 -f
 
+
+ipyrad -p mindepth_2_2_80 -b mindepth_2_2_60
+
+
+# Modify the parameters of the created file
+sed -i '/\[21] /c\'44'  ## [21] ' params-mindepth_2_2_60.txt
+
+# Run the assembly since step 4 until 7
+ipyrad -p params-mindepth_2_2_60.txt -s 7 -f
